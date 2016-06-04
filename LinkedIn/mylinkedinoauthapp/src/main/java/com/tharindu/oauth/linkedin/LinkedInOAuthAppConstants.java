de6fa2a6784b28012ac195b640257e6b6d1f6331
thariyarox <thariyarox@gmail.com>
2016-06-04 18:21:46 +0530

    Adding LinkedIn OAuth2 Java sample for retrieving user profile information

---
+++ LinkedIn/mylinkedinoauthapp/src/main/java/com/tharindu/oauth/linkedin/LinkedInOAuthAppConstants.java
@@ -0,0 +1,10 @@
+package com.tharindu.oauth.linkedin;
+
+public class LinkedInOAuthAppConstants {
+
+    public static final String TOKEN_ENDPOINT = "https://www.linkedin.com/uas/oauth2/accessToken";
+    public static final String GRANT_TYPE = "authorization_code";
+    public static final String REDIRECT_URI = "http://localhost:8080/mylinkedinoauthapp/callback";
+    public static final String CLIENT_ID = "XXXXXXXXXXX";
+    public static final String CLIENT_SECRET = "XXXXXXXXXXX";
+}
